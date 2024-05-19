4cb21e59989e8b131655aa46e8d170daa0e99e79
Mika Äärilä <maarila@gmail.com>
2024-05-19 09:57:43 +0300

    Return exercise 8.8

---
+++ library-frontend-main/src/components/Authors.jsx
@@ -0,0 +1,31 @@
+const Authors = (props) => {
+  if (!props.show) {
+    return null
+  }
+
+  const authors = props.authors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((author) => (
+            <tr key={author.name}>
+              <td>{author.name}</td>
+              <td>{author.born}</td>
+              <td>{author.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
